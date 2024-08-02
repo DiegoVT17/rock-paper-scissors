21c6535f2e64341cde608f58257ca1413a77c9c1
Diego Vera <173311520+DiegoVT17@users.noreply.github.com>
2024-08-01 23:45:31 -0500

    add possible results and scores

--- index.js
+++ index.js
@@ -11,3 +11,34 @@ function getHumanChoice() {
 
   return playerMove.toLowerCase();
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(human, computer) {
+  if (human === "paper" && computer === "scissors") {
+    console.log("Scissors cuts paper, point for the computer!");
+    computerScore += 1;
+  } else if (human === "rock" && computer === "scissors") {
+    console.log("Rock beats scissors, point for you!");
+    humanScore += 1;
+  } else if (human === "scissors" && computer === "scissors") {
+    console.log("It's a tie, no one gets a point!");
+  } else if (human === "paper" && computer === "paper") {
+    console.log("It's a tie, no one gets a point!");
+  } else if (human === "rock" && computer === "paper") {
+    console.log("Paper covers rock, point for the computer!");
+    computerScore += 1;
+  } else if (human === "scissors" && computer === "paper") {
+    console.log("Scissors cuts paper, point for you!!");
+    humanScore += 1;
+  } else if (human === "paper" && computer === "rock") {
+    console.log("Paper covers rock, point for you!");
+    humanScore += 1;
+  } else if (human === "rock" && computer === "rock") {
+    console.log("It's a tie, no one gets a point!");
+  } else if (human === "scissors" && computer === "rock") {
+    console.log("Rock beats scissors, point for the computer!");
+    computerScore += 1;
+  }
+}
