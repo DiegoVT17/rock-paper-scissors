ff68d4313af62827b9cc32b754a9e508aa07c34d
Diego Vera <173311520+DiegoVT17@users.noreply.github.com>
2024-08-01 23:44:38 -0500

    add computer choice

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello world!");
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3);
+  const computerChoice =
+    randomNumber === 0 ? "rock" : randomNumber === 1 ? "paper" : "scissors";
+
+  return computerChoice;
+}
